4886bd3256818ece855cd8d7fb53178de658ae1a
nano.son <nano.son@kakaocorp.com>
2018-09-16 16:41:27 +0900

    handle Exceptions

--- src/main/java/dto_converter/DTOConverter.java
+++ src/main/java/dto_converter/DTOConverter.java
@@ -1,27 +1,50 @@
 package dto_converter;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Objects;
 
 public class DTOConverter {
-    public static <F,T> T convert(F from, Class<T> to) throws Exception {
-        T result = to.newInstance();
-
-        return convert(from, result);
+    public static <F,T> T convert(F from, Class<T> to) {
+        try {
+            T result = to.newInstance();
+            return convert(from, result);
+        } catch (InstantiationException | IllegalAccessException e) {
+            //use logger
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            //use logger
+            return null;
+        }
     }
 
-    public static <F,T> T convert(F from, T to) throws Exception {
+    public static <F,T> T convert(F from, T to) {
+        if(Objects.isNull(from) || Objects.isNull(to))
+            throw new IllegalArgumentException("convert obejct must not be null");
+
         Method[] methodsOfFromClass = from.getClass().getDeclaredMethods();
 
         for(Method method : methodsOfFromClass) {
             if(!method.getName().contains("get"))
                 continue;
+            Method setter = null;
+            try {
+                setter = to.getClass().getDeclaredMethod(
+                        method.getName().replace("get", "set"),
+                        method.getReturnType()
+                );
+            } catch (NoSuchMethodException e) {
+                //use logger
+            }
 
-            //필드를 우선으로 보고 같은 타입, 이름의 필드가 있으면 getter, setter 활용할까
-            Method setter = to.getClass().getDeclaredMethod(method.getName().replace("get", "set"), method.getReturnType());
             if(Objects.isNull(setter))
                 continue;
-            setter.invoke(to, method.invoke(from));
+            try {
+                setter.invoke(to, method.invoke(from));
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                //use logger
+            }
         }
 
         return to;
