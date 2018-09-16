ed10ed70a5e2ced3517c68bd26282126eebbd8d0
nano.son <nano.son@kakaocorp.com>
2018-09-17 00:32:29 +0900

    add callback to convert method

--- src/main/java/dto_converter/DTOConverter.java
+++ src/main/java/dto_converter/DTOConverter.java
@@ -6,9 +6,13 @@ import java.util.Objects;
 
 public class DTOConverter {
     public static <F,T> T convert(F from, Class<T> to) {
+        return convert(from, to, (Callback<F, T>) null);
+    }
+
+    public static <F,T> T convert(F from, Class<T> to, Callback<F, T> callback) {
         try {
             T result = to.newInstance();
-            return convert(from, result);
+            return convert(from, result, callback);
         } catch (InstantiationException | IllegalAccessException e) {
             //use logger
             return null;
@@ -20,8 +24,15 @@ public class DTOConverter {
     }
 
     public static <F,T> T convert(F from, T to) {
+        return convert(from, to, null);
+    }
+
+    public static <F,T> T convert(F from, T to, Callback<F, T> callback) {
         if(Objects.isNull(from) || Objects.isNull(to))
             throw new IllegalArgumentException("convert obejct must not be null");
+
+        if(Objects.nonNull(callback))
+            callback.beforeConvert(from, to);
 
         Method[] methodsOfFromClass = from.getClass().getDeclaredMethods();
 
@@ -47,6 +58,14 @@ public class DTOConverter {
             }
         }
 
+        if(Objects.nonNull(callback))
+            callback.afterConvert(from, to);
+
         return to;
+    }
+
+    public interface Callback <F,T> {
+        void beforeConvert(F from, T to);
+        void afterConvert(F from, T to);
     }
 }
--- src/test/java/DTOConverterTest.java
+++ src/test/java/DTOConverterTest.java
@@ -1,12 +1,15 @@
-import org.junit.Test;
+import static org.mockito.Mockito.*;
 
+import org.junit.Test;
+import org.mockito.Mockito.*;
 import dto_converter.DTOConverter;
+import dto_converter.DTOConverter.Callback;
 import dto_converter.entities.Example1;
 import dto_converter.entities.Example2;
 
 public class DTOConverterTest {
     @Test
-    public void testConvertWithoutCallback() {
+    public void convertWithoutCallback() {
         Example1 example1 = new Example1();
         example1.setAge(10);
         example1.setName("name");
@@ -25,5 +28,50 @@ public class DTOConverterTest {
         assert ((Integer)example1.getAge()).equals(example2.getAge());
         assert example1.getName().equals(example2.getName());
         assert example1.getTestColumn().equals(example2.getTestColumn());
+    }
+
+    @Test
+    public void convertWithCallback() {
+        Example1 example1 = new Example1();
+        example1.setAge(10);
+        example1.setName("name");
+        example1.setTestColumn("hello");
+
+        Example2 example2 = DTOConverter.convert(example1, Example2.class, new Callback<Example1, Example2>() {
+            @Override
+            public void beforeConvert(Example1 from, Example2 to) { }
+
+            @Override
+            public void afterConvert(Example1 from, Example2 to) {
+                to.setAge(to.getAge()+1);
+            }
+        });
+        assert ((Integer)(example1.getAge()+1)).equals(example2.getAge());
+        assert example1.getName().equals(example2.getName());
+        assert example1.getTestColumn().equals(example2.getTestColumn());
+    }
+
+    @Test
+    public void callback() {
+        int testAge = 10;
+        Example1 example1 = mock(Example1.class);
+        Example2 example2 = new Example2();
+
+        DTOConverter.Callback callback = mock(DTOConverter.Callback.class);
+        when(example1.getAge()).thenReturn(testAge);
+
+        doNothing().when(callback).beforeConvert(example1, example2);
+        doAnswer(invocation -> {
+            Example2 e2 = invocation.getArgument(1);
+
+            e2.setAge(e2.getAge()+1);
+            return null;
+        }).when(callback).afterConvert(example1, example2);
+
+        example2 = DTOConverter.convert(example1, example2, callback);
+
+        assert ((Integer)(example2.getAge()-1)).equals(testAge);
+        verify(callback).beforeConvert(example1, example2);
+        verify(callback).afterConvert(example1, example2);
     }
 }
