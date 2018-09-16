cacaefea2c3b6ba8143478e13d9b34d2ab138387
nano.son <nano.son@kakaocorp.com>
2018-09-16 16:56:41 +0900

    add test code

--- src/main/java/dto_converter/Main.java
+++ src/main/java/dto_converter/Main.java
@@ -6,17 +6,5 @@ import dto_converter.entities.Example2;
 public class Main {
     public static void main(String[] args) throws Exception{
         System.out.println("hellott world");
-        Example1 example1 = new Example1();
-        example1.setAge(10);
-        example1.setName("name");
-        example1.setTestColumn("hello");
-
-        Example2 example2 = DTOConverter.convert(example1, Example2.class);
-        System.out.println(example2.toString());
-
-        example2.setAge(-1);
-        example2.setName("nothing");
-        example2.setTestColumn("nothing column");
-        System.out.println(DTOConverter.<Example2, Example1>convert(example2, example1).toString());
     }
 }
--- src/test/java/DTOConverterTest.java
+++ src/test/java/DTOConverterTest.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import dto_converter.DTOConverter;
+import dto_converter.entities.Example1;
+import dto_converter.entities.Example2;
 
 public class DTOConverterTest {
+    @Test
+    public void testConvertWithoutCallback() {
+        Example1 example1 = new Example1();
+        example1.setAge(10);
+        example1.setName("name");
+        example1.setTestColumn("hello");
+
+        Example2 example2 = DTOConverter.convert(example1, Example2.class);
+        assert ((Integer)example1.getAge()).equals(example2.getAge());
+        assert example1.getName().equals(example2.getName());
+        assert example1.getTestColumn().equals(example2.getTestColumn());
+
+        example2.setAge(-1);
+        example2.setName("nothing");
+        example2.setTestColumn("nothing column");
+
+        example1 = DTOConverter.convert(example2, example1);
+        assert ((Integer)example1.getAge()).equals(example2.getAge());
+        assert example1.getName().equals(example2.getName());
+        assert example1.getTestColumn().equals(example2.getTestColumn());
+    }
 }
